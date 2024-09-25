a43698aa4019318bffd014820519c3cbbb9aa695
Jennifer Barbosa <jenniferbarbosa.tech@gmail.com>
2024-09-24 21:28:57 -0300

    Add files via upload

---
+++ Aula 01/Conversao_temperatura.js
@@ -0,0 +1,16 @@
+// Conversão de temperaturas de graus Celsius para graus Fahrenheit
+
+// Variáveis onde serão recebidas os valores das temperaturas em Celsius 
+var temp1 = -10; var temp2 = 0; var temp3 = 20; var temp4 = 50
+
+// Variáveis com o cálculo para conversão de Celsius para Fahrenheit
+var conver_temp1 = (temp1 * 1.8) + 32
+var conver_temp2 = (temp2 * 1.8) + 32
+var conver_temp3 = (temp3 * 1.8) + 32
+var conver_temp4 = (temp4 * 1.8) + 32
+
+// Saída dos dados convertidos
+console.log(`${temp1}°C (Celsius) é ${conver_temp1}°F (Fahrenheit).`)
+console.log(`${temp2}°C (Celsius) é ${conver_temp2}°F (Fahrenheit).`)
+console.log(`${temp3}°C (Celsius) é ${conver_temp3}°F (Fahrenheit).`)
+console.log(`${temp4}°C (Celsius) é ${conver_temp4}°F (Fahrenheit).`)
